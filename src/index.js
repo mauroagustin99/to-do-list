15176389763c8cbaaaa43cc3950d7035ae9e3a04
Mauro Marcosano <mauromarcosano@gmail.com>
2024-05-25 18:35:02 -0300

    Add boilerplate

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import './style.css';
-console.log('Hello webpack');
+import {
+  format,
+  addDays,
+  subDays,
+  compareAsc,
+  differenceInDays,
+} from 'date-fns';
+
+const fechaActual = new Date();
+console.log('Fecha actual:', fechaActual);
+
+// Formatear la fecha actual
+const fechaFormateada = format(fechaActual, 'yyyy-MM-dd');
+console.log('Fecha formateada:', fechaFormateada);
+
+class ToDo {
+  constructor(task, dueDate, priority, state) {
+    this.task = task;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.state = state;
+  }
+}
+
+class Note {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+  }
+}
